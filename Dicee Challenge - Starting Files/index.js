e841c4c358cdb00c8a70f537c62440a8abd6e800
Dustin Crudden <djcooped@shaw.ca>
2021-02-13 21:04:02 -0700

    Winning messages

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,5 +1,18 @@
 var randomNumber1 = Math.floor((Math.random() *6) + 1);
 var randomNumber2 = Math.floor((Math.random() *6) + 1);
+var message = document.getElementById("title");
 
 document.getElementById("right").src = "images/dice" + randomNumber1 + ".png";
 document.getElementById("left").src = "images/dice" + randomNumber2 + ".png";
+
+if(randomNumber1 > randomNumber2){
+
+  message.innerHTML = "Player 1 Wins";
+}
+else if(randomNumber1 < randomNumber2){
+  message.innerHTML = "Player 2 Wins";
+}
+
+else{
+  message.innerHTML = "Draw";
+}
