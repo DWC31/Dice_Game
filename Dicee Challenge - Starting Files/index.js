3e546a5bee458d91ae7d9bb3982815520ec30a3b
Dustin Crudden <djcooped@shaw.ca>
2021-02-13 21:22:13 -0700

    Update index.js

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -2,8 +2,8 @@ var randomNumber1 = Math.floor((Math.random() *6) + 1);
 var randomNumber2 = Math.floor((Math.random() *6) + 1);
 var message = document.getElementById("title");
 
-document.getElementById("right").src = "images/dice" + randomNumber1 + ".png";
-document.getElementById("left").src = "images/dice" + randomNumber2 + ".png";
+document.getElementById("right").src = "images/dice" + randomNumber2 + ".png";
+document.getElementById("left").src = "images/dice" + randomNumber1 + ".png";
 
 if(randomNumber1 > randomNumber2){
 
